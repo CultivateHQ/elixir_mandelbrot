1c3b850fdf37d41808fc914c5d42270ca2de32b8
Dan Munckton <dan@cultivatehq.com>
2015-11-19 16:24:00 +0000

    Simplify the iterate function by wrapping in a facade

--- lib/mandelbrot.ex
+++ lib/mandelbrot.ex
@@ -1,15 +1,11 @@
 defmodule Mandelbrot do
   @max_iterations 10
 
-  def iterate(c, z, iterations \\ 1)
-  def iterate(_c, _z, iterations) when iterations < 1 or iterations > @max_iterations do
-    msg = "iterations must be 1 < x <= #{@max_iterations} but was #{iterations}"
-    raise ArgumentError, message: msg
-  end
-  def iterate(_c, _z, @max_iterations), do: @max_iterations
-  def iterate(c, z, iterations) when z < 2 do
+  def iterate(c, z), do: iterate(c, z, 1)
+  defp iterate(_c, _z, @max_iterations), do: @max_iterations
+  defp iterate(c, z, iterations) when z < 2 do
     IO.puts("#{c}, #{z}, #{iterations}")
     iterate(c, (z * z) + c, iterations + 1)
   end
-  def iterate(_c, _z, iterations), do: iterations
+  defp iterate(_c, _z, iterations), do: iterations
 end
