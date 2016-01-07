a2d6814477304ea641ff7d71126c426e706e860e
Dan Munckton <dan@cultivatehq.com>
2016-01-07 16:29:19 +0000

    Rename generate to calculate_iterations

--- lib/mandelbrot.ex
+++ lib/mandelbrot.ex
@@ -11,7 +11,7 @@ defmodule Mandelbrot do
     defstruct h: 0, w: 0
   end
 
-  def generate(complex_plane, grid, max_iterations \\ 10) do
+  def calculate_iterations(complex_plane, grid, max_iterations \\ 10) do
     xs = Sequence.finite_lazy(grid.h, complex_plane.re0, complex_plane.re1)
     ys = Sequence.finite_lazy(grid.w, complex_plane.im0, complex_plane.im1)
 
@@ -25,6 +25,6 @@ defmodule Mandelbrot do
   def test(max_iterations) do
     complex_plane = %ComplexPlane{re0: -2, im0: -2, re1: 2, im1: 2}
     grid = %Rectangle{h: 400, w: 400}
-    generate(complex_plane, grid, max_iterations)
+    calculate_iterations(complex_plane, grid, max_iterations)
   end
 end
