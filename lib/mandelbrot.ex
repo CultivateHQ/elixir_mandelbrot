f5924afba04ed0ef459d1989eac57d722612172b
Dan Munckton <dan@cultivatehq.com>
2016-01-07 17:15:21 +0000

    Add minimal documentation

--- lib/mandelbrot.ex
+++ lib/mandelbrot.ex
@@ -4,13 +4,31 @@ defmodule Mandelbrot do
   """
 
   defmodule ComplexPlane do
+    @moduledoc """
+    Represents a complex plane.
+
+    This is essentially two complex numbers interpreted as two points to
+    represent a rectangular area.
+    """
     defstruct re0: 0, im0: 0, re1: 0, im1: 0
   end
 
   defmodule Rectangle do
+    @moduledoc """
+    Represents a rectangle defined by a width and a height.
+    """
     defstruct h: 0, w: 0
   end
 
+  @doc """
+  For each point in `complex_plane` mapped onto the rectangular `grid`,
+  calculates the number of iterations needed to disprove Mandelbrot set
+  membership.
+
+  `max_iterations` limits how many iterations will be attempted, and thus will
+  be the maximum number returned for those numbers whose membership cannot be
+  disproven. I.e. these numbers *are* members of the Mandelbrot set.
+  """
   def calculate_iterations(complex_plane, grid, max_iterations \\ 10) do
     xs = Sequence.finite_lazy(grid.h, complex_plane.re0, complex_plane.re1)
     ys = Sequence.finite_lazy(grid.w, complex_plane.im0, complex_plane.im1)
@@ -22,6 +40,8 @@ defmodule Mandelbrot do
     end
   end
 
+  @doc false
+  # Temporary manual test driver.
   def test(max_iterations) do
     complex_plane = %ComplexPlane{re0: -2, im0: -2, re1: 2, im1: 2}
     grid = %Rectangle{h: 400, w: 400}
